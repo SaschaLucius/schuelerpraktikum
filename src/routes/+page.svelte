845150c08ecbe8891c2654cabcbfe5d308802a20
Sascha Lucius <sascha.lucius@datameer.com>
2024-01-18 15:41:31 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,14 @@
+<script lang="ts">
+	import { base } from '$app/paths';
+</script>
+
 <h1>Schülerpraktikum</h1>
 
 <h3>Taschenrechner</h3>
-<a href="/v0">Nur HTML</a>
+<a href="{base}/v0">Nur HTML</a>
 <br />
-<a href="/v1">HTML und JS</a>
+<a href="{base}/v1">HTML und JS</a>
 <br />
-<a href="/v2">HTML, CSS und JS</a>
+<a href="{base}/v2">HTML, CSS und JS</a>
 <br />
-<a href="/v3">Svelte Components</a>
+<a href="{base}/v3">Svelte Components</a>
